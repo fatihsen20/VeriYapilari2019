64e4b9eb80b2057a13b788624e0bc4550fae040c
Fatih Şen <fatih.sn2000@gmail.com>
2019-10-09 14:37:47 +0300

    Create dsdsds.java

---
+++ dsdsds.java
@@ -0,0 +1,60 @@
+public class LabDoublyLinkedList<T> extends DoublyLinkedList<T> {
+    /**
+     * Çift bağlı listenin istenilen konumuna eleman ekler
+     * @param index konum
+     * @param value eklenecek eleman
+     */
+    @Override
+    public void insertAt(int index, T value) {
+        if(index==0)
+            addFirst(value);
+        else if(index==size())
+            addLast(value);
+        else{
+            Node<T> node=getHead();
+            int i=0;
+            for(T deger:this){
+                node=node.next;
+                if (++i==index)
+                    break;
+            }
+            Node<T> newNode=new Node<T>(value,node,node.previous);
+            node.previous.next=newNode;
+            if (node.next!=null)
+                node.next.previous=newNode;
+            setSize(size()+1);
+            
+        }
+
+    }
+
+    /**
+     * Verilen konumdaki elemanı siler
+     * @param index silinecek elemanın konumu
+     * @return silinen değer
+     */
+    @Override
+    public T removeAt(int index) {
+        if (index==0)
+            return removeFirst();
+        else if (index==size()-1)
+            return removeLast();
+        else{
+            Node<T> node=getHead();
+            int i=0;
+            for (T deger:this){
+                node=node.next;
+                if (++i==index)
+                    break;
+            }
+            T sDeger=node.value;
+            node.previous.next=node.next;
+            if (node.next!=null)
+                node.next.previous=node.previous;
+            setSize(size()-1);
+            return sDeger;
+            
+        }
+        
+    }
+}
